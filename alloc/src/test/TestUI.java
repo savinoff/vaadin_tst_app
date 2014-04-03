1bb34732d20c5876d35c91ab4285f91ab0ccf798
Dmitry Savin <savin77@gmail.com>
2014-04-03 19:27:15 +0400

    ttt3

--- alloc/src/test/TestUI.java
+++ alloc/src/test/TestUI.java
@@ -1,5 +1,19 @@
 package test;
 
-public class TestUI {
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+public class TestUI extends UI {
+
+	private static final long serialVersionUID = -8893553410641968314L;
+	protected Label label;
+	
+	@Override
+	protected void init(VaadinRequest request) {
+		label = new Label("Testos");
+		setContent(label);
+	}
+
 
 }
